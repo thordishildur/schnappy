e3610288837e963b8a2135ecd7e58032f030b9cf
Þórdís Hildur Þórarinsdóttir <thordishildur@gmail.com>
2016-12-29 15:34:32 +0000

    ACCEPTED

--- chat/admin.py
+++ chat/admin.py
@@ -6,6 +6,6 @@ from chat.models import Schnap, Friendship
 class SchnapAdmin(admin.ModelAdmin):
     list_display = ('sender', 'sent_at', 'description')
 class FriendshipAdmin(admin.ModelAdmin):
-    list_display = ('creator', 'receiver', 'created')
+    list_display = ('creator', 'receiver', 'created','accepted')
 admin.site.register(Schnap, SchnapAdmin)
 admin.site.register(Friendship, FriendshipAdmin)
