2223be0f64df4208c6615c3b2b478d0d7203a9fe
Þórdís Hildur Þórarinsdóttir <thordishildur@gmail.com>
2016-12-29 15:26:35 +0000

    admin stuff

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from chat.models import Schnap, Friendship
+
+class SchnapAdmin(admin.ModelAdmin):
+    list_display = ('sender', 'sent_at', 'description')
+class FriendshipAdmin(admin.ModelAdmin):
+    list_display = ('creator', 'receiver', 'created')
+admin.site.register(Schnap, SchnapAdmin)
+admin.site.register(Friendship, FriendshipAdmin)
--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Schnap(models.Model):
+    image = models.ImageField()
+    sender = models.ForeignKey(User)
+    sent_at = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return "{}'s Schnap".format(self.sender.username)
+
+
+class Friendship(models.Model):
+    creator = models.ForeignKey(User, related_name="friendship_creator")
+    receiver = models.ForeignKey(User, related_name="friendship_receiver")
+    created = models.DateTimeField(auto_now_add=True)
+    accepted = models.BooleanField()
+
+
